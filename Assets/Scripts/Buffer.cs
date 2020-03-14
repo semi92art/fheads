d44385660d411ee0c7db3e8ba95a9009927ecacf
av_chernov <av_chernov@rambler.ru>
2020-03-14 16:46:58 +0300

    1 player in random match

--- Assets/Scripts/Buffer.cs
+++ Assets/Scripts/Buffer.cs
@@ -308,8 +308,10 @@ public class Buffer : MonoBehaviour
         int ind = Mathf.FloorToInt(((float)max_ind - 0.01f) * Random.value);
 
         SetPlayerData(ind, lg);
-
-        is2Enemies = Random.value > 0.5f ? true : false;
+        
+        //number of opponent players
+        //is2Enemies = Random.value > 0.5f ? true : false;
+        is2Enemies = false;
         oppType = Random.value > 0.5f ? OpponentType.Bycicle : OpponentType.Classic;
 
         lg = Random.value < 0.5f ? 0 : 1;
