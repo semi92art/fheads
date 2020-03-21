4e2bfabe2af9cefdde1ab067962ca0393d39cd12
av_chernov <av_chernov@rambler.ru>
2020-03-21 17:19:44 +0300

    1st version of console

---
+++ Assets/Scripts/ConsoleController.cs
@@ -0,0 +1,153 @@
+﻿using UnityEngine;
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using Boo.Lang;
+
+public delegate void CommandHandler(string[] args);
+
+public class ConsoleController {
+	
+	#region Event declarations
+	// Used to communicate with ConsoleView
+	public delegate void LogChangedHandler(string[] log);
+	public event LogChangedHandler logChanged;
+	
+	public delegate void VisibilityChangedHandler(bool visible);
+	public event VisibilityChangedHandler visibilityChanged;
+	#endregion
+
+	/// 
+	/// Object to hold information about each command
+	/// 
+	class CommandRegistration {
+		public string command { get; private set; }
+		public CommandHandler handler { get; private set; }
+		public string help { get; private set; }
+		
+		public CommandRegistration(string command, CommandHandler handler, string help) {
+			this.command = command;
+			this.handler = handler;
+			this.help = help;
+		}
+	}
+
+	/// 
+	/// How many log lines should be retained?
+	/// Note that strings submitted to appendLogLine with embedded newlines will be counted as a single line.
+	/// 
+	const int scrollbackSize = 20;
+
+	Queue<string> scrollback = new Queue<string>(scrollbackSize);
+	List commandHistory = new List();
+	Dictionary<string, CommandRegistration> commands = new Dictionary<string, CommandRegistration>();
+
+	public string[] log { get; private set; } //Copy of scrollback as an array for easier use by ConsoleView
+	
+	const string repeatCmdName = "!!"; //Name of the repeat command, constant since it needs to skip these if they are in the command history
+	
+	public ConsoleController() {
+		//When adding commands, you must add a call below to registerCommand() with its name, implementation method, and help text.
+		registerCommand("help", help, "Print this help.");
+		registerCommand("reload", reload, "Reload game.");
+		registerCommand("resetprefs", resetPrefs, "Reset & saves PlayerPrefs.");
+	}
+	
+	void registerCommand(string command, CommandHandler handler, string help) {
+		commands.Add(command, new CommandRegistration(command, handler, help));
+	}
+	
+	public void appendLogLine(string line) {
+		Debug.Log(line);
+		
+		if (scrollback.Count >= ConsoleController.scrollbackSize) {
+			scrollback.Dequeue();
+		}
+		scrollback.Enqueue(line);
+		
+		log = scrollback.ToArray();
+		if (logChanged != null) {
+			logChanged(log);
+		}
+	}
+	
+	public void runCommandString(string commandString) {
+		appendLogLine("$ " + commandString);
+		
+		string[] commandSplit = parseArguments(commandString);
+		string[] args = new string[0];
+		if (commandSplit.Length<=0)
+		{
+			return;
+		}  else if (commandSplit.Length >= 2) {
+			int numArgs = commandSplit.Length - 1;
+			args = new string[numArgs];
+			Array.Copy(commandSplit, 1, args, 0, numArgs);
+		}
+		runCommand(commandSplit[0].ToLower(), args);
+		commandHistory.Add(commandString);
+	}
+	
+	public void runCommand(string command, string[] args) {
+		CommandRegistration reg = null;
+		if (!commands.TryGetValue(command, out reg)) {
+			appendLogLine(string.Format("Unknown command '{0}', type 'help' for list.", command));
+		}  else {
+			if (reg.handler == null) {
+				appendLogLine(string.Format("Unable to process command '{0}', handler was null.", command));
+			}  else {
+				reg.handler(args);
+			}
+		}
+	}
+	
+	static string[] parseArguments(string commandString)
+	{
+		LinkedList<char> parmChars = new LinkedList<char>(commandString.ToCharArray());
+		bool inQuote = false;
+		var node = parmChars.First;
+		while (node != null)
+		{
+			var next = node.Next;
+			if (node.Value == '"') {
+				inQuote = !inQuote;
+				parmChars.Remove(node);
+			}
+			if (!inQuote && node.Value == ' ') {
+				node.Value = ' ';
+			}
+			node = next;
+		}
+		char[] parmCharsArr = new char[parmChars.Count];
+		parmChars.CopyTo(parmCharsArr, 0);
+		return (new string(parmCharsArr)).Split(new char[] {' '} , StringSplitOptions.RemoveEmptyEntries);
+	}
+
+	
+	#region Command handlers
+	//Implement new commands in this region of the file.
+
+	/// 
+	/// A test command to demonstrate argument checking/parsing.
+	/// Will repeat the given word a specified number of times.
+	///
+	///
+	
+	void reload(string[] args) {
+		Application.LoadLevel(Application.loadedLevel);
+	}
+	
+	void resetPrefs(string[] args) {
+		PlayerPrefs.DeleteAll();
+		PlayerPrefs.Save();
+	}
+
+	void help(string[] args)
+	{
+		Debug.Log("no help for dyatel");
+	}
+
+	#endregion
+}
---
+++ Assets/Scripts/ConsoleView.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Text;
+using System.Collections;
+
+public class ConsoleView : MonoBehaviour {
+    ConsoleController console = new ConsoleController();
+	
+    bool didShow = false;
+
+    public GameObject viewContainer; //Container for console view, should be a child of this GameObject
+    public Text logTextArea;
+    public InputField inputField;
+
+    void Start() {
+        if (console != null) {
+            console.visibilityChanged += onVisibilityChanged;
+            console.logChanged += onLogChanged;
+        }
+        updateLogStr(console.log);
+    }
+	
+    ~ConsoleView() {
+        console.visibilityChanged -= onVisibilityChanged;
+        console.logChanged -= onLogChanged;
+    }
+	
+    void Update() {
+        //Toggle visibility when tilde key pressed
+        if (Input.GetKeyUp("`")) {
+            toggleVisibility();
+        }
+
+        //Toggle visibility when 5 fingers touch.
+        if (Input.touches.Length == 5) {
+            if (!didShow) {
+                toggleVisibility();
+                didShow = true;
+            }
+        }  else {
+            didShow = false;
+        }
+    }
+
+    void toggleVisibility() {
+        setVisibility(!viewContainer.activeSelf);
+    }
+	
+    void setVisibility(bool visible) {
+        viewContainer.SetActive(visible);
+    }
+	
+    void onVisibilityChanged(bool visible) {
+        setVisibility(visible);
+    }
+	
+    void onLogChanged(string[] newLog) {
+        updateLogStr(newLog);
+    }
+	
+    void updateLogStr(string[] newLog) {
+        if (newLog == null) {
+            logTextArea.text = "";
+        }  else {
+            logTextArea.text = string.Join("\n", newLog);
+        }
+    }
+
+    /// 
+    /// Event that should be called by anything wanting to submit the current input to the console.
+    /// 
+    public void runCommand() {
+        console.runCommandString(inputField.text);
+        inputField.text = "";
+    }
+
+}
