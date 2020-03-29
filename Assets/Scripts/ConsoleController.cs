c1bcd766dea9307dafa681e6eea400ed0ce227eb
av_smirnov <artem@desktop.com>
2020-03-29 13:12:42 +0300

    [00011] some refactoring

--- Assets/Scripts/ConsoleController.cs
+++ Assets/Scripts/ConsoleController.cs
@@ -1,45 +1,61 @@
 ﻿using UnityEngine;
-
+using UnityEngine.SceneManagement;
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 using Boo.Lang;
 
 public delegate void CommandHandler(string[] _Args);
 
-public class ConsoleController {
+public class ConsoleController
+{
 	
 	#region Event declarations
 	
 	public delegate void LogChangedHandler(string[] _Log);
 	public event LogChangedHandler LogChanged;
-	
 	public delegate void VisibilityChangedHandler(bool _Visible);
 	public event VisibilityChangedHandler VisibilityChanged;
+	
 	#endregion
 
-
-	class CommandRegistration {
-		public string Command { get; private set; }
-		public CommandHandler Handler { get; private set; }
-		public string Help { get; private set; }
+	#region types
+	
+	private class CommandRegistration {
+		public string Command { get; }
+		public CommandHandler Handler { get; }
+		public string Help { get; }
 
 		public CommandRegistration(string _Command, CommandHandler _Handler, string _Help)
 		{
-			this.Command = _Command;
-			this.Handler = _Handler;
-			this.Help = _Help;
+			Command = _Command;
+			Handler = _Handler;
+			Help = _Help;
 		}
 	}
+	
+	#endregion
+	
+	#region constants
 
 	const int SCROLLBACK_SIZE = 100;
-
-	Queue<string> scrollback = new Queue<string>(SCROLLBACK_SIZE);
-	public List commandHistory = new List();
-	Dictionary<string, CommandRegistration> commands = new Dictionary<string, CommandRegistration>();
-
+	
+	#endregion
+	
+	#region public properties
 	public string[] Log { get; private set; } 
+	
+	#endregion
+	
+	#region readonly
+	
+	public readonly List CommandHistory = new List();
+	private readonly Queue<string> m_Scrollback = new Queue<string>(SCROLLBACK_SIZE);
+	private readonly Dictionary<string, CommandRegistration> m_Commands = new Dictionary<string, CommandRegistration>();
+	
+	#endregion
+	
+	#region consturctors
 	
 	public ConsoleController() {
 		//When adding commands, you must add a call below to registerCommand() with its name, implementation method, and help text.
@@ -50,55 +66,42 @@ public class ConsoleController {
 		RegisterCommand("clc",ClearConsole,"Clear console.");
 	}
 	
-	void RegisterCommand(string _Command, CommandHandler _Handler, string _Help) {
-		commands.Add(_Command, new CommandRegistration(_Command, _Handler, _Help));
+	#endregion
+	
+	#region private methods
+	
+	private void RegisterCommand(string _Command, CommandHandler _Handler, string _Help) {
+		m_Commands.Add(_Command, new CommandRegistration(_Command, _Handler, _Help));
 	}
 	
-	public void AppendLogLine(string _Line) {
+	private void AppendLogLine(string _Line)
+	{
 		Debug.Log(_Line);
 		
-		if (scrollback.Count >= ConsoleController.SCROLLBACK_SIZE) {
-			scrollback.Dequeue();
-		}
-		scrollback.Enqueue(_Line);
+		if (m_Scrollback.Count >= SCROLLBACK_SIZE) 
+			m_Scrollback.Dequeue();
+		m_Scrollback.Enqueue(_Line);
 		
-		Log = scrollback.ToArray();
-		if (LogChanged != null) {
+		Log = m_Scrollback.ToArray();
+		if (LogChanged != null) 
 			LogChanged(Log);
-		}
 	}
 	
-	public void RunCommandString(string _CommandString) {
-		AppendLogLine("$ " + _CommandString);
-		
-		string[] commandSplit = ParseArguments(_CommandString);
-		string[] args = new string[0];
-		if (commandSplit.Length<=0)
-		{
-			return;
-		}  else if (commandSplit.Length >= 2) {
-			int numArgs = commandSplit.Length - 1;
-			args = new string[numArgs];
-			Array.Copy(commandSplit, 1, args, 0, numArgs);
-		}
-		RunCommand(commandSplit[0].ToLower(), args);
-		commandHistory.Add(_CommandString);
-	}
-	
-	public void RunCommand(string _Command, string[] _Args) {
+	private void RunCommand(string _Command, string[] _Args)
+	{
 		CommandRegistration reg = null;
-		if (!commands.TryGetValue(_Command, out reg)) {
+		if (!m_Commands.TryGetValue(_Command, out reg)) 
 			AppendLogLine(string.Format("Unknown command '{0}', type 'help' for list.", _Command));
-		}  else {
-			if (reg.Handler == null) {
+		else 
+		{
+			if (reg.Handler == null) 
 				AppendLogLine(string.Format("Unable to process command '{0}', handler was null.", _Command));
-			}  else {
+			else 
 				reg.Handler(_Args);
-			}
 		}
 	}
 	
-	static string[] ParseArguments(string _CommandString)
+	private static string[] ParseArguments(string _CommandString)
 	{
 		LinkedList<char> parmChars = new LinkedList<char>(_CommandString.ToCharArray());
 		bool inQuote = false;
@@ -117,79 +120,70 @@ public class ConsoleController {
 		}
 		char[] parmCharsArr = new char[parmChars.Count];
 		parmChars.CopyTo(parmCharsArr, 0);
-		return (new string(parmCharsArr)).Split(new char[] {' '} , StringSplitOptions.RemoveEmptyEntries);
+		return (new string(parmCharsArr)).Split(new []{' '} , StringSplitOptions.RemoveEmptyEntries);
 	}
-
+	
+	#endregion
+	
+	#region public methods
+	
+	public void RunCommandString(string _CommandString) 
+	{
+		AppendLogLine("$ " + _CommandString);
+		
+		string[] commandSplit = ParseArguments(_CommandString);
+		string[] args = new string[0];
+		if (commandSplit.Length <= 0)
+			return;
+		
+		if (commandSplit.Length >= 2)
+		{
+			int numArgs = commandSplit.Length - 1;
+			args = new string[numArgs];
+			Array.Copy(commandSplit, 1, args, 0, numArgs);
+		}
+		RunCommand(commandSplit[0].ToLower(), args);
+		CommandHistory.Add(_CommandString);
+	}
+	
+	#endregion
+	
 	#region Command handlers
 	//Implement new commands in this region of the file.
 
-	void Reload(string[] _Args) {
+	private void Reload(string[] _Args) 
+	{
 		if (_Args.Length == 0)
-		{
-			Application.LoadLevel(Application.loadedLevel);
-		}
-		else
-		{
-			foreach (string arg in _Args)
-			{
-				if (arg == "-h")
-				{
-					AppendLogLine("Reload current level.");
-					break;
-				}
-			}
-		}
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+		else if (_Args.Any(_Arg => _Arg == "-h"))
+			AppendLogLine("Reload current level.");
 	}
 
-	void Help(string[] _Args)
+	private void Help(string[] _Args)
 	{
 		if (_Args.Length == 0)
 		{
 			Debug.Log("Print: command list");
 			AppendLogLine("Current command list:");
-			foreach (KeyValuePair<string, CommandRegistration> kvp in commands)
-			{
+			foreach (KeyValuePair<string, CommandRegistration> kvp in m_Commands)
 				AppendLogLine($"{kvp.Key}: {kvp.Value.Help}");
-			}
 		}
-		else
-		{
-			foreach (string arg in _Args)
-			{
-				if (arg == "woodpecker")
-				{
-					AppendLogLine("Work is filled up, woodpeckers!");
-					break;
-				}
-			}
-		}
+		else if (_Args.Any(_Arg => _Arg == "woodpecker"))
+			AppendLogLine("Work is filled up, woodpeckers!");
 	}
 	
-	void Restart(string[] _Args)
+	private void Restart(string[] _Args)
 	{
 		if (_Args.Length == 0)
-		{
-			Application.LoadLevel("____Preload");
-		}
-		else
-		{
-			foreach (string arg in _Args)
-			{
-				if (arg == "-h")
-				{
-					AppendLogLine("Reload game.");
-					break;
-				}
-			}
-		}
+			SceneManager.LoadScene(0);
+		else if (_Args.Any(_Arg => _Arg == "-h"))
+			AppendLogLine("Reload game.");
 	}
 
-	void Load(string[] _Args)
+	private void Load(string[] _Args)
 	{
 		if (_Args.Length == 0)
-		{
 			AppendLogLine("Specify level");
-		}
 		else
 		{
 			foreach (string arg in _Args)
@@ -202,23 +196,21 @@ public class ConsoleController {
 					AppendLogLine("level: load field.");
 					break;
 				}
-				else
+				
+				switch (arg)
 				{
-					switch (arg)
-					{
-						case "preload":
-							Application.LoadLevel("____Preload");
-							break;
-						case "menu":
-							Application.LoadLevel("____Menu");
-							break;	
-						case "level":
-							Application.LoadLevel("____Level");
-							break;
-						default:
-							AppendLogLine("No such level.");
-							break;
-					}
+					case "preload":
+						SceneManager.LoadScene(0);
+						break;
+					case "menu":
+						SceneManager.LoadScene(1);
+						break;	
+					case "level":
+						SceneManager.LoadScene(2);
+						break;
+					default:
+						AppendLogLine("No such level.");
+						break;
 				}
 			}
 		}
@@ -227,8 +219,9 @@ public class ConsoleController {
 	void ClearConsole(string[] _Args)
 	{
 		Array.Clear(Log,0,Log.Length);
-		scrollback.Clear();
-		LogChanged(Log);
+		m_Scrollback.Clear();
+		if (LogChanged != null)
+			LogChanged(Log);
 	}
 	#endregion
 }
--- Assets/Scripts/ConsoleView.cs
+++ Assets/Scripts/ConsoleView.cs
@@ -1,127 +1,106 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
-using System.Text;
-using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 
-public class ConsoleView : MonoBehaviour {
-    ConsoleController console = new ConsoleController();
-	
+public class ConsoleView : MonoBehaviour 
+{
+    #region public fields
+    
     public GameObject viewContainer; //Container for console view, should be a child of this GameObject
     public Text logTextArea;
     public InputField inputField;
     public ScrollRect consoleScrollbar;
+    
+    #endregion
 
+    #region private fields
+    
+    private readonly ConsoleController m_Console = new ConsoleController();
     private Vector3 m_SwipeFirstPosition;
     private Vector3 m_SwipeLastPosition;
     private float m_SwipeDragDistance;
-    private List<Vector3> touchPositions = new List<Vector3>();
-
+    private readonly List<Vector3> m_TouchPositions = new List<Vector3>();
     private int m_CurrentCommand;
     private int m_Index;
+    
+    #endregion
 
-    void Start() {
-        if (console != null) {
-            console.VisibilityChanged += OnVisibilityChanged;
-            console.LogChanged += OnLogChanged;
-        }
-        UpdateLogStr(console.Log);
-        m_SwipeDragDistance = Screen.width * 30 / 100;
-    }
-	
-    ~ConsoleView() {
-        console.VisibilityChanged -= OnVisibilityChanged;
-        console.LogChanged -= OnLogChanged;
-    }
-
-    void Update()
+    #region engine methods
+    
+    private void Start()
     {
+        m_Console.VisibilityChanged += OnVisibilityChanged;
+        m_Console.LogChanged += OnLogChanged;
+        UpdateLogStr(m_Console.Log);
+        m_SwipeDragDistance = Screen.width * 30 * 0.01f;
+    }
+    
+    private void OnDestroy()
+    {
+        m_Console.VisibilityChanged -= OnVisibilityChanged;
+        m_Console.LogChanged -= OnLogChanged;
+    }
+    
+    private void Update()
+    {
+        #if UNITY_EDITOR
+        
         if (Input.GetKeyUp(KeyCode.Return))
-        {
             RunCommand();
-        }
         
         //Toggle visibility when tilde key pressed
         if (Input.GetKeyUp("`"))
-        {
             ToggleVisibility();
-        }
         
         //Arrow up in history
         if (Input.GetKeyUp(KeyCode.UpArrow))
-        {
             UpCommand();
+        if (Input.GetKeyUp(KeyCode.DownArrow))
+            DownCommand();
+        
+        //Visibility on mouse swipe
+        if(Input.GetMouseButtonDown(0))
+            m_SwipeFirstPosition = new Vector2(Input.mousePosition.x,Input.mousePosition.y);
+        else if(Input.GetMouseButtonUp(0))
+        {
+            m_SwipeLastPosition = new Vector2(Input.mousePosition.x,Input.mousePosition.y);
+            if (Mathf.Abs(m_SwipeLastPosition.x - m_SwipeFirstPosition.x) > m_SwipeDragDistance)
+                ToggleVisibility();
         }
         
-        if (Input.GetKeyUp(KeyCode.DownArrow))
-        {
-            DownCommand();
-        }
-
+        #endif
+        
         //Toggle visibility when right swipe
         foreach (Touch touch in Input.touches)
         {
             if (touch.phase == TouchPhase.Moved)
-            {
-                touchPositions.Add(touch.position);
-            }
-
+                m_TouchPositions.Add(touch.position);
+            
             if (touch.phase == TouchPhase.Ended)
             {
-                m_SwipeFirstPosition = touchPositions[0];
-                m_SwipeLastPosition = touchPositions[touchPositions.Count - 1];
+                m_SwipeFirstPosition = m_TouchPositions[0];
+                m_SwipeLastPosition = m_TouchPositions[m_TouchPositions.Count - 1];
 
                 //if swipeDragDistance > 30% from screen edge
                 if (Mathf.Abs(m_SwipeLastPosition.x - m_SwipeFirstPosition.x) > m_SwipeDragDistance)
-                {
-                    if ((m_SwipeLastPosition.x < m_SwipeFirstPosition.x)) 
-                    {
-                        ToggleVisibility();
-                    }
-                    else
-                    {
-                        ToggleVisibility();
-                    }
-                }
-            }
-        }
-        
-        //Visibility on mouse swipe
-        if(Input.GetMouseButtonDown(0))
-        {
-            m_SwipeFirstPosition = new Vector2(Input.mousePosition.x,Input.mousePosition.y);
-        }
-        if(Input.GetMouseButtonUp(0))
-        {
-            m_SwipeLastPosition = new Vector2(Input.mousePosition.x,Input.mousePosition.y);
-       
-            if (Mathf.Abs(m_SwipeLastPosition.x - m_SwipeFirstPosition.x) > m_SwipeDragDistance)
-            {
-                if ((m_SwipeLastPosition.x < m_SwipeFirstPosition.x)) 
-                {
                     ToggleVisibility();
-                }
-                else
-                {
-                    ToggleVisibility();
-                }
             }
         }
     }
+    
+    #endregion
 
+    #region public methods
+    
     public void UpCommand()
     {
         m_CurrentCommand++;
-        m_Index = console.commandHistory.Count - m_CurrentCommand;
-        if ((m_Index >= 0) && (console.commandHistory.Count!=0))
-        {
-            inputField.text = console.commandHistory[m_Index].ToString();
-        }
+        m_Index = m_Console.CommandHistory.Count - m_CurrentCommand;
+        if (m_Index >= 0 && m_Console.CommandHistory.Count!=0)
+            inputField.text = m_Console.CommandHistory[m_Index].ToString();
         else
-        {
-            m_CurrentCommand = console.commandHistory.Count;
-        }
+            m_CurrentCommand = m_Console.CommandHistory.Count;
+        
         inputField.ActivateInputField();
         inputField.Select();
     }
@@ -129,11 +108,9 @@ public class ConsoleView : MonoBehaviour {
     public void DownCommand()
     {
         m_CurrentCommand--;
-        m_Index = console.commandHistory.Count - m_CurrentCommand ;
-        if (m_Index < console.commandHistory.Count)
-        {
-            inputField.text = console.commandHistory[m_Index].ToString();
-        }
+        m_Index = m_Console.CommandHistory.Count - m_CurrentCommand ;
+        if (m_Index < m_Console.CommandHistory.Count)
+            inputField.text = m_Console.CommandHistory[m_Index].ToString();
         else
         {
             inputField.text = "";
@@ -142,8 +119,21 @@ public class ConsoleView : MonoBehaviour {
         inputField.ActivateInputField();
         inputField.Select();
     }
+    
+    public void RunCommand()
+    {
+        m_Console.RunCommandString(inputField.text);
+        inputField.text = "";
+        inputField.ActivateInputField();
+        inputField.Select();
+        m_CurrentCommand = 0;
+    }
+    
+    #endregion
 
-    void ToggleVisibility()
+    #region private methods
+
+    private void ToggleVisibility()
     {
         SetVisibility(!viewContainer.activeSelf);
         consoleScrollbar.verticalNormalizedPosition = 0f;
@@ -151,43 +141,27 @@ public class ConsoleView : MonoBehaviour {
         inputField.Select();
     }
 
-    void SetVisibility(bool _Visible)
+    private void SetVisibility(bool _Visible)
     {
         viewContainer.SetActive(_Visible);
         if (inputField.text == "`")
-        {
             inputField.text = "";
-        }
     }
 
-    void OnVisibilityChanged(bool _Visible)
+    private void OnVisibilityChanged(bool _Visible)
     {
         SetVisibility(_Visible);
     }
 
-    void OnLogChanged(string[] _NewLog)
+    private void OnLogChanged(string[] _NewLog)
     {
         UpdateLogStr(_NewLog);
     }
 
-    void UpdateLogStr(string[] _NewLog)
+    private void UpdateLogStr(string[] _NewLog)
     {
-        if (_NewLog == null)
-        {
-            logTextArea.text = "";
-        }
-        else
-        {
-            logTextArea.text = string.Join("\n", _NewLog);
-        }
+        logTextArea.text = _NewLog == null ? "" : string.Join("\n", _NewLog);
     }
 
-    public void RunCommand()
-    {
-        console.RunCommandString(inputField.text);
-        inputField.text = "";
-        inputField.ActivateInputField();
-        inputField.Select();
-        m_CurrentCommand = 0;
-    }
+    #endregion
 }
